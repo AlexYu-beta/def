becde9cf739a68a00be4982c4a04dbc6d77987d4
AlexYu-beta <yym1996@gmail.com>
2017-10-17 18:10:22 +0800

    add framework of function ADD

--- def_cli.sh
+++ def_cli.sh
@@ -7,20 +7,42 @@
 
 if [ $# = 0 ];
 then
+	echo 'Err: Missing parameters.'	
 	echo 'Try "def -h" to get some help!'
 	exit -1
 fi
 
 case $1 in
+
 -h|-H|-help|-HELP)
 	cat HELP.txt
 ;;
+
 -v|-V|-version|-VERSION)
 	source ./def_conf.sh
 	echo $version
 ;;
+
+-a|-A)
+	definition=""
+	if [ $# -lt 2 ];
+	then
+		echo 'Err: Missing parameters.'
+		echo 'Try "def -h" to get some help!'
+		exit -1
+	elif [ $# = 2 ];
+	then
+		definition='No definition'
+	else
+		definition=$3
+	fi
+	echo $definition
+;;
+
 *)
+	echo 'Err: Command not found.'	
 	echo 'Try "def -h" to get some help!'
 	exit -1
 ;;
+
 esac
